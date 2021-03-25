797b36d2fae0d439ac9a7ec0ef8bea6910aa47b9
Gabriel Félix <gabrielfelix2500@gmail.com>
2021-03-25 16:48:38 -0300

    Updating files.

--- Code/main.cpp
+++ Code/main.cpp
@@ -14,7 +14,7 @@ using namespace std;
 
 int main(){
 
-	Laboratory env("Sandbox/RData_90.txt");
+	Laboratory env("Sandbox/RData_89.txt");
 
 	//env.SimpleSplitGreedyCH();
 	env.Fast_FenwickTreeSplitGreedyCH();
